fb00b104c28829525e68c138cc9ae5bc30bab1bd
yooinseo <qetu37@sookmyung.ac.kr>
2025-10-02 16:15:55 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,16 +3,33 @@
 
 int main(int argc, char *argv[])
 {
-    int x;
-    int i;
-    int sum=0;
+    int a, b;
+    char op;
+    int result;
     
-    printf("Input an integer : ");
-    scanf("%d", &x);
+    printf("Enter the calculation : ");
+    scanf("%i %c %i", &a, &op, &b);
     
-    for(i=1;i<=x;i++)
-             sum = sum + i;
-    printf("sum is %i\n", sum);
+    switch(op)
+    {
+              case '+': 
+                   result = a+b;
+                   break;
+              case '-':
+                   result = a-b;
+                   break;
+              case '*':
+                   result = a*b;
+                   break;
+              case '/':
+                   result = a/b;
+                   break;
+              default:
+                      printf("연산자가 아닙니다.\n");
+                      break; 
+    }
+    
+    printf("= %i\n", result);
   
   system("PAUSE");	
   return 0;
