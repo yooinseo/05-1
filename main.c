ba87ee6765598ecfd500620c1f5b4333fa639229
yooinseo <qetu37@sookmyung.ac.kr>
2025-10-02 15:37:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,15 @@
 int main(int argc, char *argv[])
 {
     int x;
+    int i;
+    int sum=0;
     
     printf("Input an integer : ");
     scanf("%d", &x);
     
-    if (x > 0)  
-       printf("absolute is %i.\n",x);
-    else
-        printf("absolute is %i.\n",-x);
+    for(i=1;i<=x;i++)
+             sum = sum + i;
+    printf("sum is %i\n", sum);
   
   system("PAUSE");	
   return 0;
