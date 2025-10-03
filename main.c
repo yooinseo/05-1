6ef8349bbc0b4f51f779d3b9a72d4f25b791a57d
yooinseo <qetu37@sookmyung.ac.kr>
2025-10-03 16:38:16 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,34 +3,28 @@
 
 int main(int argc, char *argv[])
 {
-    int a, b;
-    char op;
-    int result;
+    int answer = 59;
+    int num;
+    int trial = 0;
     
-    printf("Enter the calculation : ");
-    scanf("%i %c %i", &a, &op, &b);
-    
-    switch(op)
+    do
     {
-              case '+': 
-                   result = a+b;
-                   break;
-              case '-':
-                   result = a-b;
-                   break;
-              case '*':
-                   result = a*b;
-                   break;
-              case '/':
-                   result = a/b;
-                   break;
-              default:
-                      printf("연산자가 아닙니다.\n");
-                      break; 
+        printf("Input a number : ");
+        scanf("%d", &num);
+        
+        if (answer < num)
+           printf("high!\n");
+        else if (answer > num)
+             printf("low!\n");
+             
+        trial++;
+        
     }
-    
-    printf("= %i\n", result);
+    while(answer != num);
+    printf("Congratulation! trial:%i\n", trial);
+
   
   system("PAUSE");	
   return 0;
 }
+
